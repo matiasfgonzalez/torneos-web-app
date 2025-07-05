974fe98eea6843ff5535158e86c75be7f7ea5c04
Matias Gonzalez <matiasgonzalez.652@gmail.com>
2025-07-04 21:13:59 -0300

    feat: add User and News tables with migrations and update README for Prisma setup

--- README.md
+++ README.md
@@ -20,8 +20,28 @@ Instalar prisma
 npm i -D prisma
 ```
 
-Iniciar prisma
+Inicializar Prisma y crear estructura
 
 ```
 npx prisma init
 ```
+
+Esto crea el archivo prisma/schema.prisma y el .env para la conexión a la base de datos.
+
+Generar cliente Prisma
+
+```
+npx prisma generate
+```
+
+Esto genera el cliente que permite hacer consultas desde tu código TypeScript.
+
+Crear migración y aplicar a la base de datos
+
+```
+npx prisma migrate dev
+
+npx prisma migrate dev --name init
+```
+
+Esto crea una migración y sincroniza tu base de datos con los modelos del schema.
---
+++ prisma/migrations/20250705001102_new_model/migration.sql
@@ -0,0 +1,40 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "clerkUserId" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "name" TEXT,
+    "imageUrl" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "News" (
+    "id" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "summary" TEXT,
+    "content" TEXT NOT NULL,
+    "coverImageUrl" TEXT,
+    "published" BOOLEAN NOT NULL DEFAULT false,
+    "date" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "userId" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "News_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_clerkUserId_key" ON "User"("clerkUserId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateIndex
+CREATE INDEX "News_userId_idx" ON "News"("userId");
+
+-- AddForeignKey
+ALTER TABLE "News" ADD CONSTRAINT "News_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
