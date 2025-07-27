c54897114fed97bd891d14aebf79f923f78c6ea5
Matias Gonzalez <matiasgonzalez.652@gmail.com>
2025-07-27 15:03:48 -0300

    feat: add tournament categories constant

---
+++ lib/constants.ts
@@ -0,0 +1,11 @@
+export const TOURNAMENT_CATEGORIES = [
+    { label: "Reserva", value: "RESERVA" },
+    { label: "Primera", value: "PRIMERA" },
+    { label: "Veterano", value: "VETERANO" },
+    { label: "Pre-Veterano", value: "PREVETERANO" },
+    { label: "Libre", value: "LIBRE" },
+    { label: "Senior", value: "SENIOR" },
+    { label: "Sub-17", value: "SUB17" },
+    { label: "Femenino", value: "FEMENINO" },
+    { label: "Infantil", value: "INFANTIL" }
+] as const;
