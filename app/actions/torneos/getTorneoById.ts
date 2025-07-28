1c6720bc781fc71caa9eedea72696e12d3590ae2
Matias Gonzalez <matiasgonzalez.652@gmail.com>
2025-07-28 20:46:10 -0300

    Action getTorneoById

---
+++ app/actions/torneos/getTorneoById.ts
@@ -0,0 +1,20 @@
+"use server";
+
+import { ITorneo } from "@/components/torneos/types";
+import { db } from "@/lib/db";
+
+export async function getTorneoById(id: string): Promise<ITorneo | null> {
+    try {
+        const torneo = await db.tournament.findUnique({
+            where: { id },
+            include: {
+                user: true // Incluye datos del usuario creador, si es necesario
+            }
+        });
+
+        return torneo as ITorneo | null;
+    } catch (error) {
+        console.error("Error al obtener torneo por ID:", error);
+        throw error;
+    }
+}
