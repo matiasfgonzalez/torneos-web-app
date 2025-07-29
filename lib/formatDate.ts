fc1f0c8de96ae3c127c5e27c079b95d95aa86111
Matias Gonzalez <matiasgonzalez.652@gmail.com>
2025-07-29 19:47:34 -0300

    Fix formato de fechas

--- lib/formatDate.ts
+++ lib/formatDate.ts
@@ -1,4 +1,4 @@
-import { format } from "date-fns";
+import { format, addHours } from "date-fns";
 import { es } from "date-fns/locale";
 
 export const formatDate = (
@@ -6,8 +6,9 @@ export const formatDate = (
     formato: string = "dd 'de' MMMM yyyy - HH:mm"
 ): string => {
     const fecha = new Date(dateString);
+    const fechaAjustada = addHours(fecha, 3); // ⬅️ Sumar 3 horas
 
-    const resultado = format(fecha, formato, {
+    const resultado = format(fechaAjustada, formato, {
         locale: es
     });
 
