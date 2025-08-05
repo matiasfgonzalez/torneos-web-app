a19e3f7b4fc00116a506f9cdf736985e129ffea2
Matias Gonzalez <matiasgonzalez.652@gmail.com>
2025-08-05 19:35:56 -0300

    Fix build

--- components/equipos/types.ts
+++ components/equipos/types.ts
@@ -1,18 +1,19 @@
 export interface ITeam {
-    id: string;
-    name: string;
-    shortName: string;
-    description: string;
-    history: string;
-    coach: string;
-    homeCity: string;
-    yearFounded: string;
-    homeColor: string;
-    awayColor: string;
-    enabled: boolean;
-    logoUrl: string;
-    createdAt: string | Date;
-    updatedAt: string | Date;
-    tournamentId: string;
-    players: any[];
+  id: string;
+  name: string;
+  shortName: string;
+  description: string;
+  history: string;
+  coach: string;
+  homeCity: string;
+  yearFounded: string;
+  homeColor: string;
+  awayColor: string;
+  enabled: boolean;
+  logoUrl: string;
+  createdAt: string | Date;
+  updatedAt: string | Date;
+  tournamentId: string;
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  players?: any[];
 }
