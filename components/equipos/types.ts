1d3ba5bc55da72d244ef3e8e5fed7d739af8a1dc
Matias Gonzalez <matiasgonzalez.652@gmail.com>
2025-08-05 18:43:47 -0300

    Nueva interface ITeam

---
+++ components/equipos/types.ts
@@ -0,0 +1,18 @@
+export interface ITeam {
+    id: string;
+    name: string;
+    shortName: string;
+    description: string;
+    history: string;
+    coach: string;
+    homeCity: string;
+    yearFounded: string;
+    homeColor: string;
+    awayColor: string;
+    enabled: boolean;
+    logoUrl: string;
+    createdAt: string | Date;
+    updatedAt: string | Date;
+    tournamentId: string;
+    players: any[];
+}
